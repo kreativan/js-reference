7dc6870c377236381eb2cb4856eae5c72037d7c8
Ivan Milincic <kreativan@outlook.com>
2019-03-28 14:29:55 +0100

    Update snippets.js

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -1,2 +1,8 @@
 // remove php GET varibales from URL
 window.location.href =  window.location.href.split("?")[0];
+
+//
+//  Forms
+//
+// get al form fields
+var fields = document.forms["my_form_name].getElementsByTagName("input");
