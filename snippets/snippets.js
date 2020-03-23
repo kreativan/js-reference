9facae29ab7f66c7d62ca84bdf9c30e45f9bb494
Ivan Milincic <kreativan@outlook.com>
2020-03-23 18:34:14 +0100

    Update snippets.js

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -3,3 +3,8 @@ window.location.href =  window.location.href.split("?")[0];
 
 // validate form input
 if(input.checkValidity() === true) console.log("It's valid!");
+
+// do something after window load
+window.addEventListener("load", function() {
+	// Do SOmething
+}, false); 
